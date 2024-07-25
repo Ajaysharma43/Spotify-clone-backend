911c01dd5f74f0b328c535fb5019eca0f2b50dad
Ajay sharma <139312719+Ajaysharma43@users.noreply.github.com>
2024-07-25 23:31:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const allowedOrigins = [
-  'https://spotify-clone-three-ebon.vercel.app'
+  'https://spotify-clone-three-ebon.vercel.app',
   'https://spotify-clone-orpin-ten.vercel.app'
 ];
 
