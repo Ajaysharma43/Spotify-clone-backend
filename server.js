8b4108d9a9f8f95f25a45cdeb94d3baa3a80b904
Ajay sharma <139312719+Ajaysharma43@users.noreply.github.com>
2024-07-25 22:24:40 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -28,10 +28,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.post('/Login', (req, res) => {
-  // Your endpoint logic
-  res.json({ message: 'Success' });
-});
+
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://spotify-clone-orpin-ten.vercel.app/'); // Replace '*' with your frontend domain
