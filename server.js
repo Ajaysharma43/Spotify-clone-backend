3218bdd0bc094a6b27c2920b3c8113354cde27ff
Ajay sharma <139312719+Ajaysharma43@users.noreply.github.com>
2024-07-25 23:43:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('/Login',async (req, res) => {
 });
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://spotify-clone-three-ebon.vercel.app/'); // Replace '*' with your frontend domain
+  res.header('Access-Control-Allow-Origin', 'https://spotify-clone-three-ebon.vercel.app'); // Replace '*' with your frontend domain
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
